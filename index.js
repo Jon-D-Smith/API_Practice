ca1425a0c5fd797295423f7b9c18f13884289c62
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-21 16:49:47 -0500

    Added post and get route for characters

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose');
+const Character = require('./models/character');
 
+mongoose.connect('mongodb://localhost:27017/API_Practice', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('db connected')
+});
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+
+    res.send("Hello")
 })
+
+app.get('/characters', async (req, res) => {
+    const characters = await Character.find({});
+    res.send(characters)
+})
+
+app.post('/characters', async (req, res) => {
+    const character = new Character(req.body);
+    await character.save()
+    res.send(character)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
